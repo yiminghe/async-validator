4b45a733ca7cac31f4a5c1f1492224ea305e546a
yiminghe <yiminghe@gmail.com>
2015-03-10 12:44:43 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # async-validator
-validate form asynchronous
+
+Validate form asynchronous. A variation of https://github.com/freeformsystems/async-validate
+
+## differences
+
+- support ie8
+- support ``type:url`` ``type:email`` ``type:hex``
+- support nice nested object validation message
+
+- remove moment and async dependency
+- remove parallel config, defaults to true
+- remove ``type:date`` support
+- remove .error constructor
+
+## examples
+
+```
+npm install
+npm start
+```
+
+http://localhost:8010/examples/simple.html
---
+++ examples/simple.js
@@ -0,0 +1,55 @@
+var Schema = require('../');
+var schema = new Schema({
+  name: {
+    type: 'string',
+    required: true,
+    min: 5,
+    max: 10
+  },
+  address: {
+    type: 'object',
+    required: true,
+    fields: {
+      province: {
+        type: 'string',
+        required: true,
+        min: 4
+      },
+      city: {
+        type: 'string',
+        min: 5
+      },
+      async: {
+        validator: function (rule, value, callback) {
+          setTimeout(function () {
+            callback([new Error(rule.message)]);
+          }, 100);
+        },
+        message: 'address async fails'
+      }
+    }
+  },
+  async: {
+    validator: function (rule, value, callback) {
+      setTimeout(function () {
+        callback([new Error(rule.message)]);
+      }, 100);
+    },
+    message: 'async fails'
+  }
+});
+
+schema.validate({
+  name: 2,
+  address: {
+    city: 2
+  },
+  async: '2'
+}, function (errors, fields) {
+  console.log('errors');
+  console.log(errors);
+  console.log('fields');
+  console.log(fields);
+});
+
+console.log('end');
