058cf1309a78b4b235492b6ad32b370c13032018
nuintun <nuintun@qq.com>
2019-08-06 14:19:32 +0800

    :bug: Fixed first option not return Promise #168

--- __tests__/validator.spec.js
+++ __tests__/validator.spec.js
@@ -207,4 +207,215 @@ describe('validator', () => {
       );
     });
   });
+
+  describe('promise api', () => {
+    it('works', done => {
+      new Schema({
+        v: [
+          {
+            validator(rule, value, callback) {
+              callback(new Error('e1'));
+            },
+          },
+          {
+            validator(rule, value, callback) {
+              callback(new Error('e2'));
+            },
+          },
+        ],
+        v2: [
+          {
+            validator(rule, value, callback) {
+              callback(new Error('e3'));
+            },
+          },
+        ],
+        v3: [
+          {
+            validator() {
+              return false;
+            },
+          },
+          {
+            validator() {
+              return new Error('e5');
+            },
+          },
+          {
+            validator() {
+              return false;
+            },
+            message: 'e6',
+          },
+          {
+            validator() {
+              return true;
+            },
+          },
+        ],
+      })
+        .validate({
+          v: 2,
+        })
+        .catch(({ errors }) => {
+          expect(errors.length).toBe(6);
+          expect(errors[0].message).toBe('e1');
+          expect(errors[1].message).toBe('e2');
+          expect(errors[2].message).toBe('e3');
+          expect(errors[3].message).toBe('v3 fails');
+          expect(errors[4].message).toBe('e5');
+          expect(errors[5].message).toBe('e6');
+          done();
+        });
+    });
+
+    it('first works', done => {
+      new Schema({
+        v: [
+          {
+            validator(rule, value, callback) {
+              callback(new Error('e1'));
+            },
+          },
+          {
+            validator(rule, value, callback) {
+              callback(new Error('e2'));
+            },
+          },
+        ],
+        v2: [
+          {
+            validator(rule, value, callback) {
+              callback(new Error('e3'));
+            },
+          },
+        ],
+      })
+        .validate(
+          {
+            v: 2,
+            v2: 1,
+          },
+          {
+            first: true,
+          },
+        )
+        .catch(({ errors }) => {
+          expect(errors.length).toBe(1);
+          expect(errors[0].message).toBe('e1');
+          done();
+        });
+    });
+
+    describe('firstFields', () => {
+      it('works for true', done => {
+        new Schema({
+          v: [
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e1'));
+              },
+            },
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e2'));
+              },
+            },
+          ],
+
+          v2: [
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e3'));
+              },
+            },
+          ],
+          v3: [
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e4'));
+              },
+            },
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e5'));
+              },
+            },
+          ],
+        })
+          .validate(
+            {
+              v: 1,
+              v2: 1,
+              v3: 1,
+            },
+            {
+              firstFields: true,
+            },
+          )
+          .catch(({ errors }) => {
+            expect(errors.length).toBe(3);
+            expect(errors[0].message).toBe('e1');
+            expect(errors[1].message).toBe('e3');
+            expect(errors[2].message).toBe('e4');
+            done();
+          });
+      });
+
+      it('works for array', done => {
+        new Schema({
+          v: [
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e1'));
+              },
+            },
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e2'));
+              },
+            },
+          ],
+
+          v2: [
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e3'));
+              },
+            },
+          ],
+          v3: [
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e4'));
+              },
+            },
+            {
+              validator(rule, value, callback) {
+                callback(new Error('e5'));
+              },
+            },
+          ],
+        })
+          .validate(
+            {
+              v: 1,
+              v2: 1,
+              v3: 1,
+            },
+            {
+              firstFields: ['v'],
+            },
+          )
+          .catch(({ errors }) => {
+            expect(errors.length).toBe(4);
+            expect(errors[0].message).toBe('e1');
+            expect(errors[1].message).toBe('e3');
+            expect(errors[2].message).toBe('e4');
+            expect(errors[3].message).toBe('e5');
+            done();
+          });
+      });
+    });
+  });
 });
