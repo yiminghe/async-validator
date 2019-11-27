80a13db585247dfbf3f158d6cbf57abd3ea7e6b7
yiminghe <yiminghe@gmail.com>
2019-11-27 15:30:18 +0800

    clean babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,40 +1,15 @@
 console.log('Load babel config');
 
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose:true,
-        modules: false,
-      },
-    ],
-  ],
-
-  // plugins:[["@babel/plugin-transform-runtime",{
-  //   helpers:false,
-  //
-  // }],
-  //   ["module-resolver", {
-  //     "root": ["./src"],
-  //     "alias": {
-  //       "@babel/runtime/regenerator": "regenerator-runtime",
-  //     }
-  //   }]
-  // ],
-  env: {
-    test: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: true,
-            },
-          },
-        ],
+module.exports = (api) => {
+  return ({
+    presets: [
+      [
+        '@babel/preset-env',
+        (api.env() !== 'test') ? ({
+          loose: true,
+          modules: false,
+        }) : { targets: { node: true } },
       ],
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
+    ],
+  });
 };
