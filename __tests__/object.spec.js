c656677ee7cf68764213b61e0061aaaa2da0a78d
CreGod <7351102+CreGod@user.noreply.gitee.com>
2020-05-07 17:40:36 +0800

    add test case

---
+++ __tests__/object.spec.js
@@ -0,0 +1,22 @@
+import Schema from '../src/';
+
+describe('object', () => {
+  it('works for the required object with fields in case of empty string', done => {
+    new Schema({
+      v: {
+        type: 'object',
+        required: true,
+        fields: {},
+      },
+    }).validate(
+      {
+        v: '',
+      },
+      errors => {
+        expect(errors.length).toBe(1);
+        expect(errors[0].message).toBe('v is not an object');
+        done();
+      },
+    );
+  });
+});
