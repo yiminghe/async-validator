0e9a1640aef674f6b6bdc177e8ea405724aa4fb4
yiminghe <yiminghe@gmail.com>
2021-10-15 18:54:35 +0800

    use url-regex

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,10 @@
 console.log('Load babel config');
 
 module.exports = api => {
+  api.cache(false);
   return {
     presets: [
-      [
-        '@babel/preset-env',
-        api.env('test')
-          ? { targets: { node: true } }
-          : {
-              loose: true,
-              modules: false,
-            },
-      ],
+      ['@babel/preset-env', { targets: { node: true } }],
       '@babel/preset-typescript',
     ],
   };
--- src/rule/type.ts
+++ src/rule/type.ts
@@ -1,16 +1,14 @@
 import { ExecuteRule, Value } from '../interface';
 import { format } from '../util';
 import required from './required';
+import urlRegex from 'url-regex';
 
 /* eslint max-len:0 */
 
 const pattern = {
   // http://emailregex.com/
   email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+\.)+[a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]{2,}))$/,
-  url: new RegExp(
-    '^(?!mailto:)(?:(?:http|https|ftp)://|//)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$',
-    'i',
-  ),
+  url: urlRegex({ exact: true }),
   hex: /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i,
 };
 
@@ -58,7 +56,7 @@ const types = {
     return typeof value === 'string' && !!value.match(pattern.email);
   },
   url(value: Value) {
-    return typeof value === 'string' && !!value.match(pattern.url);
+    return typeof value === 'string' && value.length <= 2048 && !!value.match(pattern.url);
   },
   hex(value: Value) {
     return typeof value === 'string' && !!value.match(pattern.hex);
