4a0f0abf85fdae0bee01280f84cf2d99f651d585
iamkun <kunhello@outlook.com>
2021-10-17 10:34:16 +0800

    Update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -4,7 +4,15 @@ module.exports = api => {
   api.cache(false);
   return {
     presets: [
-      ['@babel/preset-env', { targets: { node: true } }],
+      [
+        '@babel/preset-env',
+        api.env('test')
+          ? { targets: { node: true } }
+          : {
+              loose: true,
+              modules: false,
+            },
+      ],
       '@babel/preset-typescript',
     ],
   };
